6e010fc91ffe39fb9049dd8273c062f71a99a1d5
Nick Schneider <nick-s2000@web.de>
2018-10-18 11:29:22 +0000

    Main Classes

---
+++ Pot/src/Main.java
@@ -0,0 +1,42 @@
+import java.math.BigInteger;
+
+/**
+ * Created by Nick on 18.10.2018.
+ */
+public class Main {
+
+    int x;
+
+    public Main(int x) {
+        this.x = x;
+        loop();
+    }
+
+    private long fakultaet(long n) {
+        long a = 1;
+        for(long i = n; i > 0; i--) {
+            a *= i;
+        }
+        System.out.println(a);
+        return a;
+    }
+
+    private void loop() {
+        double a = 1;
+        double b = 1;
+        long n = 0;
+        double m = 0;
+        while(a <= b) {
+            n+=1;
+            m += 1;
+            a = fakultaet(n);
+            b = Math.pow(x, m);
+        }
+        System.out.println(n);
+    }
+
+
+    public static void main(String[] args) {
+        Main m = new Main(9);
+    }
+}
